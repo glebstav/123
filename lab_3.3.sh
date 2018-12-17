d681d7ba27e2f8d40f11f42e8abd6fb9702f9653
Stavnichii Gleb Evgenevich <stavnichii@ccfit.nsu.ru>
2018-12-17 23:48:01 +0700

    last rls

---
+++ lab_3.3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget http://factorized.net/crusoe.txt
+egrep "\sz" crusoe.txt
+egrep "[[:alpha:]]{16}" crusoe.txt
+egrep "\sa[[:alpha:]]*c\b" crusoe.txt
+egrep "\sab[^o]*\b" crusoe.txt
+rm crusoe.txt
+
+
+wget http://factorized.net/patterns.txt
+egrep "^[0-9]+$" patterns.txt
+egrep "^[[:alpha:]]+$" patterns.txt
+
+egrep  "^[ABEKMHOPCTYX][0-9][0-9][0-9][ABEKMNOPCTYX]{2}[0-9]{2,3}$" patterns.txt
+rm patterns.txt
