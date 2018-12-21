5a2bd15ef3bcbc9250cc4fc6476965092cb3c049
Stavnichii Gleb Evgenevich <stavnichii@ccfit.nsu.ru>
2018-12-21 10:08:50 +0700

    323

--- lab_3.3.sh
+++ lab_3.3.sh
@@ -11,5 +11,5 @@ wget http://factorized.net/patterns.txt
 egrep "^[0-9]+$" patterns.txt
 egrep "^[[:alpha:]]+$" patterns.txt
 
-egrep  "^[ABEKMHOPCTYX][0-9][0-9][0-9][ABEKMNOPCTYX]{2}[0-9]{2,3}$" patterns.txt
+egrep  "^[ABEKMHOPCTYX][0-9][0-9][0-9][ABEKMHOPCTYX]{2}[0-9]{2,3}$" patterns.txt
 rm patterns.txt
